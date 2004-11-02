59bd6f31be2e9034d4769f118123202166e685ad
Peder Stray <peder.stray@gmail.com>
2004-11-02 22:45:08 +0100

    Add setting for automatic sort.

--- chansort.pl
+++ chansort.pl
@@ -5,21 +5,30 @@
 use strict;
 use Irssi;
 use Irssi::Irc;
-use Data::Dumper;
-$Data::Dumper::Sortkeys = 1;
 
 # ======[ Script Header ]===============================================
 
 use vars qw{$VERSION %IRSSI};
-($VERSION) = '$Revision: 1.2 $' =~ / (\d+\.\d+) /;
+($VERSION) = '$Revision: 1.3 $' =~ / (\d+\.\d+) /;
 %IRSSI = (
-          name        => 'chansort',
-          authors     => 'Peder Stray',
-          contact     => 'peder@ninja.no',
-          url         => 'http://ninja.no/irssi/chansort.pl',
-          license     => 'GPL',
-          description => 'Sort all channel and query windows',
-         );
+	  name        => 'chansort',
+	  authors     => 'Peder Stray',
+	  contact     => 'peder@ninja.no',
+	  url         => 'http://ninja.no/irssi/chansort.pl',
+	  license     => 'GPL',
+	  description => 'Sort all channel and query windows',
+	 );
+
+# ======[ Hooks ]=======================================================
+
+# --------[ sig_sort_trigger ]------------------------------------------
+
+sub sig_sort_trigger {
+    print "tigger";
+    return unless Irssi::settings_get_bool('chansort_autosort');
+    print "sort";
+    cmd_chansort();
+}
 
 # ======[ Commands ]====================================================
 
@@ -29,8 +38,6 @@ use vars qw{$VERSION %IRSSI};
 sub cmd_chansort {
     my(@windows);
     my($minwin);
-
-    print Dumper $windows[1];
 
     for my $win (Irssi::windows()) {
 	my $act = $win->{active};
@@ -55,8 +62,8 @@ sub cmd_chansort {
     for (sort {$a->[0] cmp $b->[0]} @windows) {
 	my($key,$win) = @$_;
 	my($act) = $win->{active};
-    
-#	printf("win[%d->%d]: t[%s] [%s] [%s] {%s}\n", 
+
+#	printf("win[%d->%d]: t[%s] [%s] [%s] {%s}\n",
 #	       $win->{refnum},
 #	       $minwin,
 #	       $act->{type},
@@ -76,10 +83,19 @@ sub cmd_chansort {
 
 Irssi::command_bind('chansort', 'cmd_chansort');
 
+# --------[ Register settings ]-----------------------------------------
+
+Irssi::settings_add_bool('chansort', 'chansort_autosort', 0);
+
+# --------[ Register signals ]------------------------------------------
+
+Irssi::signal_add_last('window item name changed', 'sig_sort_trigger');
+Irssi::signal_add_last('channel created', 'sig_sort_trigger');
+Irssi::signal_add_last('query created', 'sig_sort_trigger');
+
 # ======[ END ]=========================================================
 
 # Local Variables:
 # header-initial-hide: t
 # mode: header-minor
 # end:
-
